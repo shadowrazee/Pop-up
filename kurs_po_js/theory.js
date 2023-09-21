07408e0dc23b5966e518a0d8563f7a78ff92e443
kali <kali@kali.kali>
2023-09-21 16:12:31 +0300

    first

---
+++ kurs_po_js/theory.js
@@ -0,0 +1,39 @@
+// ================== OPERATORS
+let a = 10 
+let b = 5
+let c = 32 
+
+// c = c + a
+// c = c - a
+// c = c * a
+// c = c / a
+
+// c += a
+// c -= a
+// c *=a
+// c /= a
+// c = 4 ** 2
+
+
+// =========== data types
+const  age = 29 //number
+const  name = 'Roman' // string
+const isProgrammer = true //booleam
+let x //undefined
+ //null
+// console.log(typeof NaN)
+
+
+// ======================== Priority
+const fullAge = 19 
+const birthYear = 2004
+const currentYear = 2023
+//  > < 
+const isFullAge = currentYear - birthYear < fullAge
+
+// console.log (isFullAge)
+
+const num1 = 42
+const num2 = '42'
+
+console.log(num1 === num2)
